7f9267c3170c96ae96a54659845e25a0e899560c
Shohag <shoislamNYC@gmail.com>
2023-12-17 19:13:15 -0500

    Created a Database js file to connect to my DB

---
+++ server/db.js
@@ -0,0 +1,19 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: '<username>',
+  host: 'localhost',
+  database: 'linksAPI',
+  password: '<password>',
+  port: 5432,
+})
+const getLinks = (req, res) => {
+  pool.query('SELECT * FROM favlinks ORDER BY id ASC', (error, result) => {
+    if (error) {
+      throw error
+    }
+    res.status(200).json(result.rows)
+  })
+}
+module.exports = {
+  getLinks,
+}
